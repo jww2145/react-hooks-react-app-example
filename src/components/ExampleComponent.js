c87399d6dae7d21d689dbf471e1b868d46cebc61
Joshua Wu <jww2145@columbia.edu>
2022-06-27 15:38:25 -0400

    commit tree 829946bf25a966db27b8689d783b0795388e1e04

--- src/components/ExampleComponent.js
+++ src/components/ExampleComponent.js
@@ -1,4 +1,4 @@
-import React from "react";
+
 
 // no need to modify anything in here. The second test is looking for the returned JSX from this component
 function ExampleComponent() {
@@ -9,6 +9,7 @@ function ExampleComponent() {
         alt="a deeply impressed man, perhaps thinking 'whoa'"
       />
       <p>Whoa!</p>
+
     </div>
   );
 }
